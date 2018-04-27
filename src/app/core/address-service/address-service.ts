9138a7b29098c64d9f00d017a7e3bd86e0b9017d
Isaac Johnson <isaac@ij-mbp.local>
2018-04-27 16:23:01 -0500

    switch to actual icons. see Material Design on this site https://ionicframework.com/docs/ionicons/

--- src/app/core/address-service/address-service.ts
+++ src/app/core/address-service/address-service.ts
@@ -8,8 +8,8 @@ export class AddressServiceProvider {
   constructor() {
     console.log('Hello AddressServiceProvider Provider');
 
-    this.addresses.push(new Address(1, 'YMCA', '2175 Radio Dr', '', 'Woodbury', 'MN', '55125', 'YMCA'));
-    this.addresses.push(new Address(2, 'ThomsonReuters', '610 Opperman Dr', '', 'Eagan', 'MN', '55123', 'work'));
+    this.addresses.push(new Address(1, 'YMCA', '2175 Radio Dr', '', 'Woodbury', 'MN', '55125', 'stopwatch'));
+    this.addresses.push(new Address(2, 'ThomsonReuters', '610 Opperman Dr', '', 'Eagan', 'MN', '55123', 'hammer'));
     this.addresses.push(new Address(3, 'Home', '1310 Schooner Ct', '', 'Woodbury', 'MN', '55125', 'home'));
   }
 
