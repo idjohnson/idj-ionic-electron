048a3c6f8939df81f50a4af3d6b10c97e1b5561f
isaac <isaac.johnson@gmail.com>
2018-05-13 13:53:00 -0500

    new files

--- src/app/core/address-service/address-service.ts
+++ src/app/core/address-service/address-service.ts
@@ -1,20 +1,62 @@
 import { Injectable } from '@angular/core';
+import 'rxjs';
+import 'rxjs/add/operator/map';
+import { SetupService } from '../setup-service';
+import { DispatcherService } from '../dispatcher-service';
+import { ElectronIpcService } from '../electronipc-service';
 import { Address } from '../address';
+
 
 @Injectable()
 export class AddressServiceProvider {
+  // private addresses: Address[];
 
-  private addresses: Address[] = [];
-  constructor() {
-    console.log('Hello AddressServiceProvider Provider');
-
-    this.addresses.push(new Address(1, 'YMCA', '2175 Radio Dr', '', 'Woodbury', 'MN', '55125', 'stopwatch', '', '44.916907', '-92.932066'));
-    this.addresses.push(new Address(2, 'ThomsonReuters', '610 Opperman Dr', '', 'Eagan', 'MN', '55123', 'hammer', '', '44.824317', '-93.112396'));
-    this.addresses.push(new Address(3, 'Home', '1310 Schooner Ct', '', 'Woodbury', 'MN', '55125', 'home', '', '44.930511', '-92.911950'));
+  constructor(
+    private setupService: SetupService,
+    private dispatcher: DispatcherService,
+    private ipc: ElectronIpcService) {
   }
 
-  fetchAll() : Promise<Address[]>{
-    var p = Promise.resolve<Address[]>(this.addresses);
-    return p;
+  // Async version
+  async fetchAll(): Promise<Address[]> {
+    let person = await this.setupService.fetchPerson();
+    let addresses = await this.dispatcher
+      .getAddressesByPhone(person.phone);
+    this.ipc.updateLocationsMenu(addresses);
+    return addresses;
+  }
+
+  // Non async version
+  // fetchAll(): Promise<Address[]> {
+  //   return this.setupService.fetchPerson()
+  //   .then(person => {
+  //     return this.dispatcher.getAddressesByPhone(person.phone)
+  //     .then(addresses => {
+  //       this.ipc.updateLocationsMenu(addresses);
+  //       return addresses;
+  //     });
+  //   });
+  // }
+
+  validate(address: Address): Promise<Address> {
+    return this.dispatcher.validateAddress(address);
+  }
+
+  async add(address: Address): Promise<Address> {
+    let person = await this.setupService.fetchPerson();
+    address.endpoint = {
+      callerName: person.name,
+      did: person.phone
+    };
+
+    return this.dispatcher.addAddress(address);
+  }
+
+  provision(address: Address): Promise<boolean> {
+    return this.dispatcher.provisionAddress(address);
+  }
+
+  remove(address: Address): Promise<boolean> {
+    return this.dispatcher.removeAddress(address);
   }
 }
--- src/app/core/address.ts
+++ src/app/core/address.ts
@@ -2,7 +2,7 @@ import { Endpoint } from "./endpoint";
 
 export class Address {
   // These fields come from the Dispatcher API
-  public addressId: number | string = 8;
+  public addressId: number | string = 0;
   public addressLine1: string = '';
   public addressLine2: string = '';
   public houseNumber: string = '';
@@ -25,8 +25,8 @@ export class Address {
 
 
   constructor(
-    address1: string = '',
-    address2: string = '',
+    addressLine1: string = '',
+    addressLine2: string = '',
     community: string = '',
     state: string = '',
     postalCode: string = '',
@@ -47,115 +47,7 @@ export class Address {
   }
 
   toString() : string {
-    let result = `${this.addressLine1||''} ${this.addressLine2||''} ${this.community||''}, ${this.state} ${this.zip}`;
+    let result = `${this.addressLine1||''} ${this.addressLine2||''} ${this.community||''}, ${this.state||''} ${this.postalCode||''}`;
     return result;
   }
 }
-
-/*
-getAddressesByPhone(phone: string): Promise<Address[]> {
-  const url = `${dispatcherApi.url}/GetAddressesByDid/${phone}`;
-
-  return this.http.get(url)
-    .retry(2)
-    .map(x => {
-      var result: Address[] = x.json();
-      return result;
-    })
-    .toPromise();
-}
-
-validateAddress(address: Address): Promise<Address> {
-  const url = `${dispatcherApi.url}/ValidateAddress`;
-
-  return this.http.post(url, address)
-    .map(x => {
-      let result: Address = x.json();
-      return result;
-    }).toPromise();
-}
-
-addAddress(address: Address): Promise<Address> {
-  const url = `${dispatcherApi.url}/AddAddress`;
-
-  return this.http.post(url, address)
-  .map(x => {
-    let result: Address = x.json();
-    return result;
-  }).toPromise();
-}
-
-provisionAddress(address: Address): Promise<boolean> {
-  const url = `${dispatcherApi.url}/ProvisionAddress`;
-  var headers = new Headers();
-  this.createHeaders(headers);
-
-  return this.http.post(url, address.addressId, { headers: headers })
-    .map(x => {
-      return true;
-    }).toPromise();
-}
-
-createHeaders(headers: Headers) {
-  headers.append('Authorization', dispatcherApi.auth);
-  headers.append('Accept', 'application/json');
-}
-
-removeAddress(address: Address): Promise<boolean> {
-  const url = `${dispatcherApi.url}/RemoveAddress/${address.addressId}`;
-  return this.http.delete(url)
-    .map(x => true)
-    .toPromise();
-}
-
-//ipc
-updateLocations(addresses: Address[]) {
-  if (this.electron.ipcRenderer) {
-    this.electron.ipcRenderer.send('Locations', addresses);
-  }
-}
-
-//setup
-fetchPerson(): Promise<Person> {
-  var p = Promise.resolve<Person>(this.person);
-  return p;
-}
-
-setPerson(person: Person): Promise<Person> {
-  this.person.name = person.name;
-  this.person.phone = person.phone;
-  return this.fetchPerson();
-}
-
-//address service
-async fetchAll(): Promise<Address[]> {
-  let person = await this.setupService.fetchPerson();
-  let addresses = await this.dispatcher
-    .getAddressesByPhone(person.phone);
-  this.ipc.updateLocationsMenu(addresses);
-  return addresses;
-}
-
-validate(address: Address): Promise<Address> {
-  return this.dispatcher.validateAddress(address);
-}
-
-async add(address: Address): Promise<Address> {
-  let person = await this.setupService.fetchPerson();
-  address.endpoint = {
-    callerName: person.name,
-    did: person.phone
-  };
-
-  return this.dispatcher.addAddress(address);
-}
-
-provision(address: Address): Promise<boolean> {
-  return this.dispatcher.provisionAddress(address);
-}
-
-remove(address: Address): Promise<boolean> {
-  return this.dispatcher.removeAddress(address);
-}
-
-*/
